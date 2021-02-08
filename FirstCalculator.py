66da434d14b1b91d022d9a7df1458b6011816820
weaustin <weaustin@iu.edu>
2021-02-08 13:43:41 -0500

    Converted simple operations to functions.

--- FirstCalculator.py
+++ FirstCalculator.py
@@ -1,14 +1,28 @@
 x = int(input('Please input the first number: '))
 y = int(input('Please input the second number: '))
-operator = input('Please enter an operator (+, -, *, or /): ')
+operator = input('Please enter an operator (+, -, *, /, or ^): ')
 ans = 0
 
+#define operators
+def plus(x, y):
+    return x + y
+def minus(x, y):
+    return x - y
+def multiplication(x,y):
+    return x * y
+def division(x,y):
+    return x / y
+def power(x,y):
+    return x ** y
+
+#process input, direct to proper function
 if operator == '+':
-    ans = x + y
+    print(plus(x,y))
 elif operator == '-':
-    ans = x - y
+    print(minus(x,y))
 elif operator == '*':
-    ans = x * y
+    print(multiplication(x,y))
 elif operator == '/':
-    ans = x / y
-print(x, operator, y, '=', ans)
+    print(division(x,y))
+elif operator == '^':
+    print(power(x,y))
