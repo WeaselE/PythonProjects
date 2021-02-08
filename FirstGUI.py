808016e299989802574cb5b8b2dd058f1fd01007
weaustin <weaustin@iu.edu>
2021-02-08 14:03:41 -0500

    Button added to FirstGUI

--- FirstGUI.py
+++ FirstGUI.py
@@ -1,10 +1,22 @@
 import tkinter as tk
 
+#Window initialized
 window = tk.Tk()
+
+#Window 1 with just text
 greeting = tk.Label(text="Hello World",
                     foreground="white",
                     background="black",
                     width=10,
-                    height=10)
+                    height=3)
 greeting.pack()
+window.mainloop()
+
+#Window 2 with just a button
+button = tk.Button(text="Click Here",
+                    foreground="black",
+                    background="red",
+                    height=4,
+                    width=20)
+button.pack()
 window.mainloop()
