54ad35bf920ac812f513838aae765190c5631d87
weaustin <weaustin@iu.edu>
2021-02-08 13:59:33 -0500

    First Draft of FirstGUI, label created.

---
+++ FirstGUI.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+window = tk.Tk()
+greeting = tk.Label(text="Hello World",
+                    foreground="white",
+                    background="black",
+                    width=10,
+                    height=10)
+greeting.pack()
+window.mainloop()
